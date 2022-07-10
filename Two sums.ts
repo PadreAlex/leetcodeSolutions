8d5281c81aa9ba95a23bae113857bd213d57bc29
PadreAlex <67380935+PadreAlex@users.noreply.github.com>
2022-07-10 18:47:05 +0300

    Create Two sums.ts

---
+++ Two sums.ts
@@ -0,0 +1,17 @@
+function twoSum(nums: number[], target: number): number[] {
+    var hash = new Map();
+    
+    for(let i = 0; i < nums.length; i++) {
+        let curRun = nums[i];
+        
+        let needed_number = target - curRun;
+        
+        if (hash.has(needed_number)) {
+            
+            let num = hash.get(needed_number)
+            return [i, num]
+        }
+        
+        hash.set(curRun, i)
+    }
+};
