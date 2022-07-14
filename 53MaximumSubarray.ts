f03065cf6b6707d0625029f70bd0a50d0b2ade84
PadreAlex <67380935+PadreAlex@users.noreply.github.com>
2022-07-15 01:28:26 +0300

    Create 53MaximumSubarray.ts

---
+++ 53MaximumSubarray.ts
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/maximum-subarray/
+function maxSubArray(nums: number[]): number {
+    let res: number = nums[0];
+    let max: number = nums[0];
+    
+    for(let i = 1; i < nums.length; i++) {
+        max = Math.max(nums[i], max +nums[i]);
+        res = Math.max(max, res);
+    }
+    return res;
+};
